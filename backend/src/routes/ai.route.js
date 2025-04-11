5313b6709266c137ff7c1f8859f60ae05fc86706
Prakash singh <singhprakash545@gmail.com>
2025-04-11 15:43:23 +0530

    protected AI Chat

--- backend/src/routes/ai.route.js
+++ backend/src/routes/ai.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { AIChat } from '../controllers/ai.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router()
 
-router.post("/aichat",AIChat);
+router.post("/aichat",protectRoute,AIChat);
 
 export default router;
